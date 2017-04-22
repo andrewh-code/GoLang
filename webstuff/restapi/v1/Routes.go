404a86a2288e66cc84de0778d0b627237f9758aa
andrewh-code <andrewh.code@gmail.com>
2017-04-22 00:54:01 -0400

    created api versioning
    added api version for URL
    /api/v1blahblahblah

---
+++ webstuff/restapi/v1/Routes.go
@@ -0,0 +1,50 @@
+package main
+
+import "net/http"
+
+type Route struct {
+	Name        string
+	Method      string
+	Pattern     string
+	HandlerFunc http.HandlerFunc
+}
+
+type Routes []Route
+
+//versioning variable
+var version = "v1"
+
+// create a new file just to handle the routes themselves
+// this file focuses on the paths while the router.go file focuses on driving the routes
+var routes = Routes{
+	Route{
+		"Index",
+		"Get",
+		"/api/{version}/",
+		Index,
+	},
+	Route{
+		"TodoIndex",
+		"GET",
+		"/api/{version}/todos",
+		ToDoIndex,
+	},
+	Route{
+		"TodoShow",
+		"GET",
+		"/api/{version}/todos/{todoID}",
+		ToDoShow,
+	},
+	Route{
+		"TodoCreate",
+		"GET", //try to reach page but get 404, because it's post? - correct ()chagne back to post
+		"/api/{version}/todocreate",
+		TodoCreate,
+	},
+	Route{
+		"TodoDelete",
+		"GET",
+		"/api/{version}/tododelete/{todoID}",
+		TodoDelete,
+	},
+}
