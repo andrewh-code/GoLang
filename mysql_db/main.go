e25b766d06f3fcaf2f487600e5853529df70b810
andrewh-code <andrewh.code@gmail.com>
2017-04-28 12:32:23 -0400

    updates to mysql_db

--- mysql_db/main.go
+++ mysql_db/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"database/sql"
 	"fmt"
+	"strconv"
 
 	_ "github.com/go-sql-driver/mysql"
 )
@@ -33,28 +34,46 @@ func main() {
 	//db insert
 	sqlInsert := "INSERT INTO user (username, password, email, date) values ('andrew', 'password', 'andrew@email.com', '2017-01-01')"
 	fmt.Println(sqlInsert)
-	// sqlQuery := "SELECT * FROM " + DB_TABLE
-	// sqlDelete := "DELETE FROM user WHERE username='andrew'"
+	sqlQuery := "SELECT * FROM " + DB_TABLE
+	sqlDelete := "DELETE FROM user WHERE username='andrew'"
 
-	stmtInsert, err := dbc.Exec(sqlInsert)
+	stmtInsert, err := dbc.Prepare("INSERT INTO user (username, password, email, date) values (?, ?, ?, ?)")
 	if err != nil {
 		panic(err)
 	}
-	fmt.Println("stmtInsert", stmtInsert)
+	dbcResult, err := stmtInsert.Exec("andre", "password123", "andre@gmail.com", "2017-01-01")
+	if err != nil {
+		panic(err) //turn this into its own function
+	}
+	id, err := dbcResult.LastInsertId()
 
-	// //query result
-	// stmtQuery, err := dbc.Query(sqlQuery)
-	// if err != nil {
-	// 	panic(err)
-	// }
-	// fmt.Println("stmtQuery is: ", stmtQuery)
+	fmt.Println("dbcResult: ", id)
 
-	// //delete
-	// stmtDelete, err := dbc.Exec(sqlDelete)
-	// if err != nil {
-	// 	panic(err)
-	// }
-	// fmt.Println("stmtDelete is: ", stmtDelete)
+	//query result
+	rows, err := dbc.Query(sqlQuery)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("stmtQuery is: ", rows)
+
+	// go through the results
+	for rows.Next() {
+		var uid int
+		var username string
+		var password string
+		var email string
+		var date string
+
+		err = rows.Scan(&uid, &username, &password, &email, &date)
+		fmt.Println(strconv.Itoa(uid) + "\t" + username + "\t" + password + "\t" + email + "\t" + date)
+	}
+
+	//delete
+	stmtDelete, err := dbc.Exec(sqlDelete)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("stmtDelete is: ", stmtDelete)
 
 	//close the database connection
 	dbc.Close()
