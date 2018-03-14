d733440eb9601f5b723c19da109607b6d26cc4cb
andrewh-code <andrewh.code@gmail.com>
2018-03-14 00:35:49 -0400

    minor updates to properties file class

--- zz_goparity/utilities/properties/properties.go
+++ zz_goparity/utilities/properties/properties.go
@@ -4,7 +4,10 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"strings"
 )
+
+const CommentSymbol = "#"
 
 type Properties map[string]string
 
@@ -16,12 +19,14 @@ func (properties *Properties) ReadProperties(fileName string) (Properties, error
 	// check to see if the file exists
 	if _, err := os.Stat(fileName); err != nil {
 		fmt.Println("Error: ", fileName, " does not exist")
+		return nil, err
 	}
 
 	// try to open the file
 	file, err := os.Open(fileName)
 	if err != nil {
 		fmt.Println("Error: Unable to open file: ", fileName)
+		return nil, err
 	}
 
 	defer file.Close()
@@ -34,7 +39,15 @@ func (properties *Properties) ReadProperties(fileName string) (Properties, error
 	// if key does exist, insert it into hash map
 
 	for scanner.Scan() {
-		fmt.Println(scanner.Text())
+		// check if line has a comment (skip it) OR if line does not contain equal sign
+		// should return error if = is not present in a line (not a true properties file then)
+		line := scanner.Text()
+		if strings.Contains(line, CommentSymbol) || !(strings.Contains(line, "=")) {
+			continue
+		}
+		// check to make sure that the equal sign is NOT at the beginning of the line
+
+		fmt.Println(line)
 	}
 
 	return outProp, err
